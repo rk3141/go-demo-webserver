e17f2f1c4ace472cac6ee59682c1f3633b296a4d
rishit khandelwal <rishitkh@gmail.com>
2024-06-11 15:46:22 +0530

    add abilty to use css files

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+var contentType map[string]string = make(map[string]string)
 
 type Page struct {
 	Path  string
@@ -15,7 +18,7 @@ type Page struct {
 }
 
 func loadFile(filename string) (*Page, error) {
-	path := "./public/" + filename + ".html"
+	path := "./public" + filename
 	text, err := os.ReadFile(path)
 
 	if err != nil {
@@ -28,15 +31,22 @@ func loadFile(filename string) (*Page, error) {
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 
+	fmt.Printf("req: '%s'\n", path)
+
 	if path == "/" {
-		path = "/index"
+		path = "/index.html"
 	}
 
-	txt, err := loadFile(path[1:])
+	txt, err := loadFile(path)
 	if err != nil {
 		fmt.Fprintf(w, "Path '%s' not found", path)
 		return
 	}
+
+	s := strings.Split(path, ".")
+	extension := s[len(s)-1]
+
+	w.Header().Add("Content-Type", contentType[extension])
 	fmt.Fprint(w, string(txt.Body))
 }
 
@@ -45,12 +55,17 @@ type HelloPage struct {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+
 	t, _ := template.ParseFiles("./public/hello.html")
 	name := r.URL.Path[len("/hello/"):]
 	t.Execute(w, HelloPage{Name: name})
 }
 
 func main() {
+
+	contentType["html"] = "text/html"
+	contentType["css"] = "text/css"
+
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/hello/", helloHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
