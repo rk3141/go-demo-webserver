920301c0ea9f0832e1321db2c81964b43d4e23b4
rishit khandelwal <rishitkh@gmail.com>
2024-06-11 13:37:34 +0530

    Add basic file server functionality

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+func loadFile(filename string) ([]byte, error) {
+	text, err := os.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return text, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if path == "/" {
+		path = "/index.html"
+	}
+
+	txt, err := loadFile("./public" + path)
+	if err != nil {
+		fmt.Fprintf(w, "Path '%s' not found", path)
+		return
+	}
+	fmt.Fprint(w, string(txt))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
