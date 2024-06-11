604c1c0999068b98859cbe92d2999ed17b440fab
rishit khandelwal <rishitkh@gmail.com>
2024-06-11 15:16:57 +0530

    Using go html/template for dynamic pages

--- main.go
+++ main.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-func loadFile(filename string) ([]byte, error) {
-	text, err := os.ReadFile(filename)
+type Page struct {
+	Path  string
+	Title string
+	Body  []byte
+}
+
+func loadFile(filename string) (*Page, error) {
+	path := "./public/" + filename + ".html"
+	text, err := os.ReadFile(path)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return text, nil
+	return &Page{Path: path, Title: filename, Body: text}, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 
 	if path == "/" {
-		path = "/index.html"
+		path = "/index"
 	}
 
-	txt, err := loadFile("./public" + path)
+	txt, err := loadFile(path[1:])
 	if err != nil {
 		fmt.Fprintf(w, "Path '%s' not found", path)
 		return
 	}
-	fmt.Fprint(w, string(txt))
+	fmt.Fprint(w, string(txt.Body))
+}
+
+type HelloPage struct {
+	Name string
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("./public/hello.html")
+	name := r.URL.Path[len("/hello/"):]
+	t.Execute(w, HelloPage{Name: name})
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello/", helloHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
